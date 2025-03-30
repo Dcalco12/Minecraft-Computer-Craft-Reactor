014831fadde0a52d9305651f6a50f658810eef9b
Dcalco12 <dcalcorn7@gmail.com>
2025-03-30 09:11:18 -0400

    Update printing.lua

--- Monitor Computer/printing.lua
+++ Monitor Computer/printing.lua
@@ -5,13 +5,22 @@ local function printPower()
     monitor.setTextColor(1)
     monitor.setTextScale(1)
 
-    if reactorActive
+    if reactorActive == true
     then
     monitor.setCursorPos(1, 1)
     monitor.clearLine()
     monitor.write("Reactor: On")
+    end
 
-    else
+    if reactorActive == false
+    then
+    monitor.setCursorPos(1, 1)
+    monitor.clearLine()
+    monitor.write("Reactor: Off")
+    end
+
+    if reactorActive == nil
+    then
     monitor.setCursorPos(1, 1)
     monitor.clearLine()
     monitor.write("Reactor: Off")
@@ -22,28 +31,75 @@ local function printPower()
 
     monitor.setCursorPos(1, 4)
     monitor.clearLine()
-    monitor.write(string.format("%d",currentPower) .. "/" .. maxPower)
+
+    if currentPower ~= nil
+    then
+        if maxPower ~= nil
+        then monitor.write(string.format("%d",currentPower) .. "/" .. maxPower)
+        end
+    end
+
+    if currentPower == nill 
+    then if maxPower ~= nill
+        then monitor.write("nil" .. "/" .. maxPower)
+        end
+    end
+    
+    if maxPower == nil
+    then if currentPower ~= nil
+        then monitor.write(currentPower .. "/" .. "nil")
+        end
+    end
+
+    if currentPower == nil
+    then
+        if maxPower == nil
+        then monitor.write("nil" .. "/" .. "nil")
+        end
+    end
+     
 
     monitor.setCursorPos(1, 6)
     monitor.write("Battery Percent:")
 
     monitor.setCursorPos(1, 7)
     monitor.clearLine()
-    monitor.write(string.format("%.2f", percentPower) .. "%")
+
+    if percentPower ~= nil
+    then monitor.write(string.format("%.2f", percentPower) .. "%")
+    end
+
+    if percentPower == nil
+    then monitor.write("nil")
+    end
 
     monitor.setCursorPos(1, 9)
     monitor.write("RF Per Tick:")
 
     monitor.setCursorPos(1, 10)
     monitor.clearLine()
-    monitor.write(string.format("%.2f", rfPerTick))
+
+    if rfPerTick == nil
+    then monitor.write("nil")
+    end
+
+    if rfPerTick ~= nil
+    then monitor.write(string.format("%.2f", rfPerTick))
+    end
     
     monitor.setCursorPos(1, 12)
     monitor.write("Waste Percent:")
 
     monitor.setCursorPos(1, 13)
     monitor.clearLine()
-    monitor.write(string.format("%.2f", percentWaste) .. "%")
+
+    if percentWaste == nil
+    then monitor.write("nil")
+    end
+
+    if percentWaste ~= nil
+    then monitor.write(string.format("%.2f", percentWaste) .. "%")
+    end
     
     if currentFuel == 0
     then
@@ -54,10 +110,11 @@ local function printPower()
     else
         monitor.setCursorPos(1, 14)
         monitor.clearLine()
-        if monitor.getCursorBlink()
-        then monitor.setCursorBlink(false) end
-    end
 
+        if monitor.getCursorBlink()
+        then monitor.setCursorBlink(false)
+        end
+    end
 end
 
 t.printPower = printPower
